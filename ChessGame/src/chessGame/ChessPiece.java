b4dded3d81d8f657d1414a06c6199069663a3517
EvaHung000 <evahung000@gmail.com>
2015-10-11 01:15:14 +0800

    Update ChessPiece.java

--- ChessGame/src/chessGame/ChessPiece.java
+++ ChessGame/src/chessGame/ChessPiece.java
@@ -12,7 +12,13 @@ public abstract class ChessPiece {
 	public String getPosition() {
 		return this.position;
 	}
-
+	
+	/*If get available move is needed
+	public abstract String[] getAvailableMoves();
+	I am not quite sure how it works...moveIsAvailable call getAvailableMovies() for checking the movie is available?
+	So both is abstruct? As the function for getting different chess available moves are different......
+	Or, Only IsAvailable(String move) is needed?
+	*/
 	/**
 	 * 
 	 * @param move
@@ -26,6 +32,16 @@ public abstract class ChessPiece {
 	public void updatePosition(String move) {
 		// TODO - implement ChessPiece.updatePosition
 		throw new UnsupportedOperationException();
+		boolean start = moveIsAvailable(move);
+		try{
+			if (start){
+				this.position = move;
+			}
+			else{
+				move = null;
+				this.position = move;
+			}
+		}//Which class handle the unavailable move? chess controller? boolean updatePosition?
 	}
 
 }
