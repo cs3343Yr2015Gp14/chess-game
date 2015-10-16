66ebd96347e976575c8ad56683ce9941b454cc8a
ConnieIp <conniekamanip@gmail.com>
2015-10-17 02:03:42 +0800

    Add id and playerScore

--- ChessGame/src/chessGame/ChessPlayer.java
+++ ChessGame/src/chessGame/ChessPlayer.java
@@ -3,9 +3,24 @@ package chessGame;
 public class ChessPlayer {
 
 	private String name;
+	private int id;
+	private int playerScore;
 
-	public ChessPlayer(String playerName) {
+	public ChessPlayer(String playerName,int id) {
 		this.name=playerName;
+		this.id=id;
+	}
+	
+	public int getPlayerScore(){
+		return playerScore;
+	}
+
+	public int getPlayerId(){
+		return id;
+	}
+	
+	public String getPlayerName(){
+		return name;
 	}
 
 }
