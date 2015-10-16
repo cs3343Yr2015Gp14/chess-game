aa3bf2fca475a0e7a891d5538443e273e0dd62de
frodogu <frodogu940928@gmail.com>
2015-10-16 18:32:10 +0800

    update constructor ChessPlayer()

--- ChessGame/src/chessGame/ChessPlayer.java
+++ ChessGame/src/chessGame/ChessPlayer.java
@@ -2,15 +2,10 @@ package chessGame;
 
 public class ChessPlayer {
 
-	private int id;
 	private String name;
-	/**
-	 * 
-	 * @param id
-	 */
-	public ChessPlayer(String id, String name) {
-		this.id=id;
-		this.name=name;
+
+	public ChessPlayer(String playerName) {
+		this.name=playerName;
 	}
 
 }
