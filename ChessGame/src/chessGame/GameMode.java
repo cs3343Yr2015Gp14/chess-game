d0c69779309b0975531cb01a078fe2beb4f80bec
EvaHung000 <evahung000@gmail.com>
2015-10-23 16:12:17 +0800

    Update and rename Result.java to GameMode.java

--- ChessGame/src/chessGame/GameMode.java
+++ ChessGame/src/chessGame/GameMode.java
@@ -2,4 +2,6 @@ package chessGame;
 
 public interface Result{
 	public String getResult(ChessPlayer player1, ChessPlayer player2);
+        public boolean isEndGame();
+        public void addScore(ChessPlayer player);
 }
