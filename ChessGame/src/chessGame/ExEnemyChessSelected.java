1a5fecdebfe93af9c9bd128e22200377466b1811
scordlau <skh.kh@hotmail.com>
2015-11-01 22:26:59 +0800

    Create ExEnemyChessSelected.java

---
+++ ChessGame/src/chessGame/ExEnemyChessSelected.java
@@ -0,0 +1,11 @@
+package chessGame;
+
+public class ExEnemyChessSelected extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3319443552946642956L;
+	public ExEnemyChessSelected() {super("Selected piece does not belong to you!");}
+
+}
